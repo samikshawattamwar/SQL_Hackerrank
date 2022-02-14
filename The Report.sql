81d392be7dfd60bd53a4932e0cf848de609ea222
samikshawattamwar <77261236+samikshawattamwar@users.noreply.github.com>
2022-02-14 17:08:36 +0530

    Create The Report.sql
    You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
    
    
    
    Grades contains the following data:
    
    
    
    Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
    
    Write a query to help Eve.

---
+++ The Report.sql
@@ -0,0 +1,8 @@
+select CASE
+        WHEN Grades.Grade <8 THEN 'NULL'
+        ELSE Students.Name
+        END
+, Grades.Grade, Students.Marks
+FROM Students, Grades
+WHERE Students.Marks >= Grades.Min_mark AND Students.Marks <= Grades.Max_mark 
+ORDER BY Grades.Grade DESC, Students.Name;
